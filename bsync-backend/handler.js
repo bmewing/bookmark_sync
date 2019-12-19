ea38f9ac6a82116ac3dec77a80a4af111da5d38d
Mark Ewing <mewing@eastman.com>
2019-12-19 14:19:23 -0500

    started working on lambda backend

--- bsync-backend/handler.js
+++ bsync-backend/handler.js
@@ -1,4 +1,25 @@
 'use strict';
+const AWS = require('aws-sdk');
+const git = require('isomorphic-git');
+const fs = require('fs');
+
+git.plugins.set('fs', fs);
+const s3 = new AWS.S3()
+
+async function download_bookmarks(key) {
+    let params = {
+        Bucket: "bsync-backend",
+        Key: key+"/bookmarks.json"
+    };
+    s3.getObject(params, (err, data) => {
+        let res = err ? "ERROR" : data;
+        return res
+    })
+}
+
+async function initialize() {
+    await git.init({dir: '' })
+}
 
 module.exports.hello = async event => {
   return {
